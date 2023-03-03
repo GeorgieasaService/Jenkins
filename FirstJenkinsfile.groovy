2e4cbf6e889c8877781ee076bf3fee72c54643c5
GeorgeRBarnes <georgieintech@gmail.com>
2023-03-03 15:27:33 +0200

    Indentation corrections

--- FirstJenkinsfile.groovy
+++ FirstJenkinsfile.groovy
@@ -5,26 +5,27 @@ pipeline {
         jdk 'OracleJDK8'
     }
     stages {
-        stage('fetching the repo'){
-            step{
-                git branch: 'vp-rem' url:'https://github.com/devopshydclub/vprofile-repo.git'
+        stage('fetching the repo') {
+            steps{
+                git branch: 'vp-rem', url: 'https://github.com/devopshydclub/vprofile-repo.git'
             }
         }
-        stage('Build'){
-            step{
+        stage('Build') {
+            steps {
                 sh 'mvn install -DskipTests'
                }
-            post{
+            post {
                 success {
                     echo 'Now Archiving the artifacts'
                     archiveArtifacts artifacts: '**/target/*.war'
                 }
             }
-        stage('unit test')
-            steps{
+        }
+        stage('UNIT TEST') {
+            steps {
                 sh 'mvn test'
             }
         }
-
+        }
     }
 }
